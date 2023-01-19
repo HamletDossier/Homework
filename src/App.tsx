f606a5338746007f86c1e47643bb75e4470a6f84
Hamlet <dossier@hamletideas.com>
2023-01-19 13:50:55 -0600

    Add mobile design light

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import CrossIcon from "./components/icons/CrossIcon";
+import MoonIcon from "./components/icons/MoonIcon";
 const App = () =>{
   return (
     <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat bg-gray-300 min-h-screen">
       <header className="container mx-auto px-4 pt-8">
         <div className="flex justify-between">
           <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.3em]">Todo</h1>
-          <button>luna</button>
+          <button><MoonIcon className=" fill-red-400" /></button>
         </div>
         <form className="bg-white rounded-md overflow-hidden py-4 flex gap-4 items-center px-4 mt-8">
           <span className="rounded-full border-2 h-5 w-5 inline-block"></span>
@@ -15,44 +17,44 @@ const App = () =>{
             />
         </form>
       </header>
-      <main className="container mx-auto px-4">
-        <article>
-            <button>Circulo</button>
-            <p>Complet App</p>
-            <button>Equis</button>
-        </article>
-        <article>
-            <button>Circulo</button>
-            <p>Complet App</p>
-            <button>Equis</button>
-        </article>
-        <article>
-            <button>Circulo</button>
-            <p>Complet App</p>
-            <button>Equis</button>
-        </article>
-        <article>
-            <button>Circulo</button>
-            <p>Complet App</p>
-            <button>Equis</button>
-        </article>
-        <article>
-            <button>Circulo</button>
-            <p>Complet App</p>
-            <button>Equis</button>
-        </article>
+      <main className="container mx-auto px-4 mt-8">
+        <div className="bg-white rounded-md [&>article]:p-4">
+          <article className="flex gap-4 border-b border-b-gray-400">
+              <button className="rounded-full border-2 h-5 w-5 inline-block flex-none"></button>
+              <p className="text-gray-600 grow">Complet App</p>
+              <button className="flex-none"><CrossIcon /></button>
+          </article>
+          <article className="flex gap-4 border-b border-b-gray-400">
+              <button className="rounded-full border-2 h-5 w-5 inline-block flex-none"></button>
+              <p className="text-gray-600 grow">Complet App</p>
+              <button className="flex-none"><CrossIcon /></button>
+          </article>
+          <article className="flex gap-4 border-b border-b-gray-400">
+              <button className="rounded-full border-2 h-5 w-5 inline-block flex-none"></button>
+              <p className="text-gray-600 grow">Complet App</p>
+              <button className="flex-none"><CrossIcon /></button>
+          </article>
+          <article className="flex gap-4 border-b border-b-gray-400">
+              <button className="rounded-full border-2 h-5 w-5 inline-block flex-none"></button>
+              <p className="text-gray-600 grow">Complet App</p>
+              <button className="flex-none"><CrossIcon /></button>
+          </article>
 
-        <section>
-          <span>5 items left</span>
-          <button>Clear Completed</button>
-        </section>
+          <section className="py-4 px-4 flex justify-between">
+            <span className=" text-gray-400">5 items left</span>
+            <button className="text-gray-400">Clear Completed</button>
+          </section>
+          
+        </div>
       </main>
-      <section className="px-4 container mx-auto">
-          <button>All</button>
-          <button>Active</button>
-          <button>Completed</button>
+      <section className="px-4 container mx-auto mt-8">
+        <div className="bg-white p-4 rounded-md flex justify-center gap-4">
+          <button className="text-blue-500">All</button>
+          <button className="hover:text-blue-500">Active</button>
+          <button className="hover:text-blue-500">Completed</button>
+        </div>
       </section>
-      <p className="text-center">Drag and drop to reorder list</p>
+      <p className="text-center mt-8">Drag and drop to reorder list</p>
     </div>
   );
 };
