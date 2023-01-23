7fd3cb094021b0e4ba530bac72468c76da86622a
Hamlet <dossier@hamletideas.com>
2023-01-23 08:55:11 -0600

    Add TodoCreate functionality

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,16 @@ const initialStateTodos = [
   
 ]
 const App = () =>{
-  const [todos, setTodos] = useState(initialStateTodos)
+  const [todos, setTodos] = useState(initialStateTodos);
+  const createTodo = (title:string) =>{
+    const newTodo = {id:Date.now(),title, completed:false}
+    setTodos([...todos,newTodo]);
+  }
   return (
     <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat bg-gray-300 min-h-screen">
       <Header />
       <main className="container mx-auto px-4 mt-8">
-        <TodoCreate />
+        <TodoCreate createTodo={createTodo}/>
         <TodoList todos={todos} />
         <TodoComputed />
         <TodoFilter />
--- src/components/TodoCreate.tsx
+++ src/components/TodoCreate.tsx
@@ -1,11 +1,25 @@
-const TodoCreate = () => {
+import { useState } from "react";
+
+const TodoCreate = ({createTodo}) => {
+  const [title,setTitle] = useState('');
+
+const handleSubmitAddTodo = (e:any)=>{
+  e.preventDefault();
+  if(!title.trim())
+    return setTitle("");
+  createTodo(title);
+  setTitle("");
+}
+
   return (
-    <form className="bg-white rounded-md overflow-hidden py-4 flex gap-4 items-center px-4">
+    <form onSubmit={handleSubmitAddTodo} className="bg-white rounded-md overflow-hidden py-4 flex gap-4 items-center px-4">
       <span className="rounded-full border-2 h-5 w-5 inline-block"></span>
         <input 
         type="text" 
         placeholder="Create a new Todo.."
         className="w-full text-gray-400 outline-none"
+        value={title}
+        onChange ={(e)=> setTitle(e.target.value)}
         />
     </form>
 );
